b55df70ce5a3b8f510b0d248f40b1ddaad95287d
Alexander Kochergin <ejiqpep@gmail.com>
2017-12-09 13:24:15 +0100

    WIP

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello Vue!</h1>
+    <my-user />
+  </div>
+</template>
+
+<script>
+import user from './User.vue'
+
+export default {
+    components: {
+      myUser: user
+    }
+}
+</script>
---
+++ src/User.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h3>Alex</h3>
+    <div>
+      This is our admin
+    </div>
+  </div>
+</template>
