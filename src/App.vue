3179151aeb4de9a6956d65bbd4ef3afd838a5f2c
Alexander Kochergin <ejiqpep@gmail.com>
2017-12-30 15:07:56 +0100

    Passing props from child to parent

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div>
     <h1>Hello Vue!</h1>
-    <my-user
-      v-for='user in users'
-      :name='user.name'
-      :description='user.description'
-      :is-active='user.isActive'
-      :key='user.name'
-    />
-    <my-user />
+    <loginForm :onLogin='onLogin' />
   </div>
 </template>
 
 <script>
-import user from './User.vue'
-
+import loginForm from './LoginForm.vue'
 export default {
   data () {
     return {
-      users: [
-        {name: 'Alex', description: 'This is admin', isActive: true},
-        {name: 'Jack', description: 'This is manager', isActive: false},
-        {name: 'John', description: 'This is client', isActive: false}
-      ]
     }
   },
   components: {
-    myUser: user
+    loginForm
+  },
+  methods: {
+    onLogin (data) {
+      console.log('child component said login', data)
+    }
   }
 }
 </script>
---
+++ src/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>Login form</h3>
+    <div>
+      <div>
+        <label>Email:</label>
+        <input type='text' v-model='email' />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type='password' v-model='password' />
+      </div>
+      <div>
+        <button @click='login'>Login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['onLogin'],
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      this.onLogin({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
